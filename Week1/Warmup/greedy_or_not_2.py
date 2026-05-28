22c517c42497f159a9c7eb246aefba90da931126
saksham <sakshamjindal434@gmail.com>
2026-05-28 16:03:53 +0530

    An alternative similar algo added

---
+++ Week1/Warmup/greedy_or_not_2.py
@@ -0,0 +1,29 @@
+
+def maxscore(n,mylist,turn):
+    if n==1 and turn:
+        return mylist[0]
+    elif n==1:
+        return -mylist[0]
+    if n==2 and turn:
+        return max(mylist[0],mylist[1])-min(mylist[0],mylist[1])
+    elif n==2:
+        return min(mylist[0],mylist[1])-max(mylist[0],mylist[1])
+    newlist1 = mylist.copy()
+    newlist2 = mylist.copy()
+    newlist1.pop(0)
+    newlist2.pop(n-1)
+    if turn:
+        return max(maxscore(n-1,newlist1,False)+mylist[0],maxscore(n-1,newlist2,False)+mylist[n-1])
+    else :
+        return -maxscore(n,mylist,True)
+    
+n = int(input())
+a = input()
+mylist = list(map(int,a.split()))
+score1 = maxscore(n,mylist,True)
+if score1>0:
+    print("Player 1 wins")
+elif score1<0:
+    print("Player 2 wins")
+else:
+    print("Its a draw")
